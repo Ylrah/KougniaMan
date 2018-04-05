b29b2afa56b6e9030466ec1130e01c35f611f5ed
v.malleron <v.malleron@gmail.com>
2018-04-06 00:29:23 +0200

    hero called from api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,25 +3,34 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 
+interface Hero {
+  id: number;
+  name: string;
+  image: string;
+
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  title = 'KougniaMan !';
-  results = '';
+
+  private heroes = [];
 
   constructor(private http: HttpClient) {
 
   }
 
   ngOnInit(): void {
-    this.http.get('https://akabab.github.io/superhero-api/api//id/1.json').subscribe(data => {
-      console.log(data);
+    this.http.get<Hero[]>('https://akabab.github.io/superhero-api/api/all.json').subscribe(heroes => {
+      this.heroes = heroes;
     });
+
   }
+
+
+
 }
 
-// import HttpClient & methode & cdn api.
