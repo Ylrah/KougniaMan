9bc8f55273d2d5b4b3a8c47d7ceb6b17ab006cca
v.malleron <v.malleron@gmail.com>
2018-04-05 19:38:07 +0200

    api called

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'KougniaMan';
+  results = '';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.http.get('https://akabab.github.io/superhero-api/api//id/1.json').subscribe(data => {
+      console.log(data);
+    });
+  }
 }
+
+// import HttpClient & methode & cdn api.
