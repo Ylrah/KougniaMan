d0f7687ed406972cbb2c1934f62c6043cc6cd07b
Ylrah <harly.wcs@gmail.com>
2018-04-06 12:03:35 +0200

    deploy.sh v2

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-npm run build -- --base-href "https://ctoxy.github.io/SuperFightHeroes/"
-npm run deploy -- --repo=https://github.com/ctoxy/SuperFightHeroes.git
+npm run build -- --base-href "https://ctoxy.github.io/KougniaMan/"
+npm run deploy -- --repo=https://github.com/ctoxy/KougniaMan.git
